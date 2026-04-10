a6b03c3359b5e5a25d880f55c58c54b14db25a86
yallappat-spec <yallappa.t@kushals.com>
2026-04-10 16:50:08 +0530

    Connect Google Sheets backend URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
  * ============================================================
  */
 const CONFIG = {
-  APPS_SCRIPT_URL: 'PASTE_YOUR_APPS_SCRIPT_URL_HERE',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwDQP6uVT0UJCiQkSoQRmRkBy9kewvJ7IhlyZNgNJs2qRX4WSHxXS4lLo6G3J5c_zFUcQ/exec',
 
   // Department name shown in the sidebar (optional)
   DEPARTMENT_NAME: 'My Department'
